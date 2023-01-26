39049b3a2bf005442673e9dcaad694e838cc1745
Miyamoto Musashi <business.dmjacob@gmail.com>
2023-01-26 17:42:33 -0500

    added the rest of the code for the current date's weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,32 @@
-// Acceptance Criteria
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
-// WHEN I view the UV index
-// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
-
-
-// on click of search button take the value of the input field and console.log the data return of that specific city.
-var searchBtn = document.querySelector(".search-btn");
+let searchBtn = document.querySelector(".search-btn");
+var searchedEl = document.querySelector(".searched-cities");
 
 function findCity(event) {
     event.preventDefault();
-    var appKey = "5d0479bcef657d162bba17e27204ff00";
-    var locationEl = document.querySelector("#input-city").value;
-    var units = "imperial";
-    var cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${locationEl}&appid=${appKey}&units=${units}`;
-    var currentDate = new Date().toLocaleDateString('en-us', { year: "numeric", day: "numeric", month: "short" });
+    const appKey = "5d0479bcef657d162bba17e27204ff00";
+    const locationEl = document.querySelector("#input-city").value;
+    const units = "imperial";
+    const cityURL = `https://api.openweathermap.org/data/2.5/weather?q=${locationEl}&appid=${appKey}&units=${units}`;
+    const currentDate = new Date().toLocaleDateString('en-us', { year: "numeric", day: "numeric", month: "short" });
+    const currNameDateEl = document.querySelector(".location-date");
+    const currTemperatureEl = document.querySelector(".current-temperature");
+    const currFeelsLike = document.querySelector(".current-feels-like");
+    const currHumidtyEl = document.querySelector(".current-humidity");
+    const currWindSpeed = document.querySelector(".current-wind-speed");
+    const fiveDayURL = `api.openweathermap.org/data/2.5/forecast?q=${locationEl}&appid=${appKey}`
 
     fetch(cityURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            // once loaded populate the current weather information to the webpage based on what city was searched
-            var currNameDateEl = document.querySelector(".location-date");
-            var currTemperatureEl = document.querySelector(".current-temperature");
-            var currHumidtyEl = document.querySelector(".current-humidity");
+            //once the data is processed I want to store whatever value came from locationEl into cities array
             currNameDateEl.textContent = `${data.name} (${currentDate})`
             currTemperatureEl.textContent = `Temperature: ${data.main.temp}°F`;
+            currFeelsLike.textContent = `Feels Like: ${data.main.feels_like}°F`
             currHumidtyEl.textContent = `Humidity: ${data.main.humidity}%`;
-
-        })
+            currWindSpeed.textContent = `Wind Speed: ${data.wind.speed}mph`;
+        });
 }
 
 searchBtn.addEventListener("click", findCity);
